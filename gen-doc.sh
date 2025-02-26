a5c2ad3657ab90469170b479b5b099dc9c815953
Djavan Bertrand <djavan.bertrand@octopuscommunity.com>
2025-02-26 15:00:28 +0100

    Added gen-doc script

---
+++ gen-doc.sh
@@ -0,0 +1,13 @@
+DOCC_JSON_PRETTYPRINT="YES"
+REPO_NAME=HexaCommunity
+
+xcodebuild docbuild \
+    -scheme 'HexaCommunity-Package' \
+    -destination 'generic/platform=iOS' \
+    -derivedDataPath ./docs_build_files
+
+(xcrun --find docc) process-archive transform-for-static-hosting \
+    "docs_build_files/Build/Products/Debug-iphoneos/Hexa.doccarchive" \
+    "docs_build_files/Build/Products/Debug-iphoneos/HexaUI.doccarchive" \
+    --hosting-base-path $REPO_NAME
+    --output-path ./docs
